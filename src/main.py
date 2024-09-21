e9098e78233266d952f20b4aa0c24e2d10889bc7
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-09-21 15:48:27 +0100

    add: jwt for bearer token & passlib for hashing
    Added JWT for the bearer token and passlib for hashing the passwords to
    make sure they match with the password received on the /token endpoint.
    
    Updated the logic to make it more secure by using the new libraries
    which also means can now save the hashed password in the db.

--- src/main.py
+++ src/main.py
@@ -6,101 +6,147 @@ Documetation: https://www.github.com/florent-haxhiu/expense-tracker
 
 """
 
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
 
+import jwt
+import os
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
 
 from models.users import User, UserInDB
 
 app = FastAPI()
 
+SECRET_KEY = os.getenv("SECRET_KEY") or ""
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "fakehasheds",
+        "hashed_password": "fakehashedsecret",
         "disabled": False,
         "expenses": None,
-    },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
-        "expenses": None,
-    },
+    }
 }
 
 
-def fake_hash_password(password: str) -> str:
-    return "fakehashed" + password
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
-def get_user(db, username: str) -> UserInDB | None:
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    """Authenticate user"""
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    """Creates an encoded token
+
+    Copies the data to be encoded
+    Sets an expiry date on the token and appending it onto the data
+    Encodes everything and returns the encoded string
+    """
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_user(db: dict, username: str):
     """If username is in db then return user object in UserInDB"""
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
 
 
-def fake_decode_token(token) -> UserInDB | None:
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
-    return user
-
-
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    """Decodes token and returns user is not None"""
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str | None = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
 
 async def get_current_active_user(
     current_user: Annotated[User, Depends(get_current_user)],
 ) -> User | HTTPException:
-    """Checks if the user is active """
+    """Checks if the user is active"""
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
-@app.post("/login")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    """Allows a User to connect to the API
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
-    Read from a fake db and validate that it exists
-    Pass down all the data to the UserInDB model
-    Get the unhashed password and compare to the one in the form_data
-    Return the access token and token type if they match
 
-    Throw HTTPExceptions if the user_dict doesn't exist or if the passwords don't match
+"""Allows a User to connect to the API
 
-    .. note:
-        Unsecure version
+Read from a fake db and validate that it exists
+Pass down all the data to the UserInDB model
+Get the unhashed password and compare to the one in the form_data
+Return the access token and token type if they match
 
-    """
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    return {"access_token": user.username, "token_type": "bearer"}
+Throw HTTPExceptions if the user_dict doesn't exist or if the passwords don't match
+"""
 
 
 @app.get("/users/me")
