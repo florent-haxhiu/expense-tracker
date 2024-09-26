92bcd9f3c7540c12fdaf1f7f256c05ca7cc90fbd
florent-haxhiu <florenthaxhiu03@gmail.com>
2024-09-26 19:45:04 +0100

    add: load env vars

--- src/main.py
+++ src/main.py
@@ -5,8 +5,12 @@ Author: Florent Haxhiu
 Documetation: https://www.github.com/florent-haxhiu/expense-tracker
 
 """
+from dotenv import load_dotenv
+
 from fastapi import FastAPI
 from routes import router
+
+load_dotenv()
 
 app = FastAPI()
 
